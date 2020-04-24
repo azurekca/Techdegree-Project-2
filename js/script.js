1851dc9eb84b411e9355ced6797945bf7172a5e8
Agata Zurek <agata.w.zurek@gmail.com>
2020-04-24 13:59:53 -0700

    updated comments for showPage function

--- js/script.js
+++ js/script.js
@@ -12,11 +12,13 @@ const ITEMS_TO_SHOW = 10;
 const studentListElements = document.getElementsByClassName('student-item');
 
 /**
- * Shows the given page number of student list elements and hides the rest
- * @param {object} listElements 
- * @param {number} pageNum 
+ * Shows the student list elements for the given page number
+ * and hides the rest of the student list elements
+ * @param {object} listElements - HTMLCollection of student list items
+ * @param {number} pageNum - number of the page to show
  */
 function showPage(listElements, pageNum) {
+   // set start and end indexes of list items that will be shown
 	const startIndex = ITEMS_TO_SHOW * pageNum - ITEMS_TO_SHOW;
 	const endIndex = startIndex + ITEMS_TO_SHOW;
 	for (let i = 0; i < listElements.length; i++) {
@@ -29,8 +31,6 @@ function showPage(listElements, pageNum) {
 		}
 	}
 }
-
-showPage(studentListElements, 6);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
