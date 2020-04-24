529b600f0211ea707f07885bf55d9fe84191ed02
Agata Zurek <agata.w.zurek@gmail.com>
2020-04-24 12:14:57 -0700

    added showPage function

--- js/script.js
+++ js/script.js
@@ -2,49 +2,39 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+/**
+ *  Global variables
+ *  ITEMS_TO_SHOW - number of student list elements to show per page
+ *  studentListElements - HTMLCollection of student list items
+ */
+const ITEMS_TO_SHOW = 10;
+const studentListElements = document.getElementsByClassName('student-item');
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/**
+ * Shows the given page number of student list elements and hides the rest
+ * @param {object} listElements 
+ * @param {number} pageNum 
+ */
+function showPage(listElements, pageNum) {
+	const startIndex = ITEMS_TO_SHOW * pageNum - ITEMS_TO_SHOW;
+	const endIndex = startIndex + ITEMS_TO_SHOW;
+	for (let i = 0; i < listElements.length; i++) {
+      // show the student list items of the current page
+		if (i >= startIndex && i < endIndex) {
+         listElements[i].style.display = '';
+      // otherwise, do not display the student list item on the page
+		} else {
+			listElements[i].style.display = 'none';
+		}
+	}
+}
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
+showPage(studentListElements, 6);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
-
-
-
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
